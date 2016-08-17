eec123f47a995e18605686016c3368db03a9a92f
Christopher Morgan <wappers@googlemail.com>
2016-08-17 21:05:46 +0100

    Add static import
    Change-Id: I79c39bb57335b0b0004794b14875001f6045bc37

--- src/test/java/c/a/m/kalah/CircularCursorTest.java
+++ src/test/java/c/a/m/kalah/CircularCursorTest.java
@@ -1,9 +1,10 @@
 package c.a.m.kalah;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class CircularCursorTest {
@@ -23,18 +24,18 @@ public class CircularCursorTest {
 		items.add("eleven");
 
 		CircularCursor<String> cursor = new CircularCursor<String>(items, 0);
-		Assert.assertEquals("currentElement", "one", cursor.getCurrentElement());
-		Assert.assertEquals("currentElement", 0, cursor.getCurrentIndex());
+		assertEquals("currentElement", "one", cursor.getCurrentElement());
+		assertEquals("currentElement", 0, cursor.getCurrentIndex());
 
-		Assert.assertEquals("currentElement", "two", cursor.next());
-		Assert.assertEquals("currentElement", 1, cursor.getCurrentIndex());
+		assertEquals("currentElement", "two", cursor.next());
+		assertEquals("currentElement", 1, cursor.getCurrentIndex());
 
 		// Loop around once
 		for (int i = 0; i < 12; i++) {
 			cursor.next();
 		}
 
-		Assert.assertEquals("currentElement", "three", cursor.getCurrentElement());
-		Assert.assertEquals("currentElement", 2, cursor.getCurrentIndex());
+		assertEquals("currentElement", "three", cursor.getCurrentElement());
+		assertEquals("currentElement", 2, cursor.getCurrentIndex());
 	}
 }
